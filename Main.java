4a1c39c1c58af226680ea0172a408703d6b2ed61
Matheus Alexandre <matheusasj@live.com>
2020-11-03 21:47:02 -0300

    Função trocaParcialTime funcionando corretamente

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
     }
     public static void main(String[] args) {
         int[][] table = OpenFile.getFile();
+        
         int[][] s = {
             {0,0,0,0,0,0,0,0,0,0},
             {6,-2,4,3,-5,-4,-3,5,2,-6},
@@ -18,14 +19,16 @@ public class Main {
             {-4,5,2,-1,6,-2,1,-6,-5,4},
             {3,6,-1,-5,-2,1,5,2,-6,-3},
             {-2,-3,6,4,1,-6,-4,-1,3,2},
-            {-1,-4,-5,2,-3,6,-2,4,5,1}
+            {-1,-4,-5,2,-3,5,-2,3,4,1}
         };
         
+        // int [][]s = null;
         try{
             // s = Heuristics.geraSolucaoInicial(table);
             System.out.println("Result Table:");
             printTable(s);
-            s = Heuristics.SimulatedAnnealing(table, 10, 0.01f, (s.length - 1)*((s.length - 1)-1)/2, 0.97);
+            // s = Heuristics.SimulatedAnnealing(table, 10, 0.01f, (s.length - 1)*((s.length - 1)-1)/2, 0.97);
+            s = Heuristics.trocaParcialTime(s);
             printTable(s);
         }catch(NullPointerException npe){
             System.err.println("Não foi possível construir uma solução inicial.");
